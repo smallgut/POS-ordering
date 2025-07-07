022ec4eabd037c0c55264eb3c1a1bf001c1a88f2
smallgut <tobylau2023@gmail.com>
2025-07-08 07:40:27 +0800

    Add files via upload

--- sis-order-system/tailwind.config.js
+++ sis-order-system/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
      module.exports = {
-       content: ["./*.html"],
+       content: ["./sis-order-system/*.html"],
        theme: {
          extend: {},
        },
