976366bb0b218004cb20f4445dab14345e43072f
smallgut <tobylau2023@gmail.com>
2025-07-14 07:24:11 +0800

    Add files via upload

--- sis-order-system/vite.config.js
+++ sis-order-system/vite.config.js
@@ -3,6 +3,16 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        checkout: './checkout.html',
+        summary: './summary.html',
+        veg: './veg.html',
+        checkout_js: './checkout.js' // Include checkout.js in the build
+      }
+    }
+  },
+  envPrefix: 'VITE_' // Ensure VITE_ variables are exposed to client
 });
